b3579a307fca23f82f4a27f0b9c39d3e20e6d66b
Adrien Haxaire <adrien.haxaire@lunatech.nl>
2020-05-15 15:27:19 +0200

    Download AsciiDoc files from Github

--- build.py
+++ build.py
@@ -4,9 +4,10 @@ def copy_files():
     tree = requests.get("https://api.github.com/repos/lunatech-labs/lunatech-blog/git/trees/f2a3bd4398e3c1e6fe3f90a13a48ad51a2562160")
     github_files = [item['path'] for item in tree.json()["tree"]]
     for github_file in github_files:
-        raw_file = requests.get('https://raw.githubusercontent.com/lunatech-labs/lunatech-blog/master/posts' + github_file)
+        raw_file = requests.get('https://raw.githubusercontent.com/lunatech-labs/lunatech-blog/master/posts/' + github_file)
         with open('tech/_posts/' + github_file, 'w') as local_file:
             local_file.write(raw_file.text)
+            print('Downloaded an moved {0}'.format(github_file))
 
 if __name__ == "__main__":
     copy_files()
