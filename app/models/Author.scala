1128b166fab3fcf66c7dcc42f672880a6fe0346c
Willem Jan Glerum <willem.jan.glerum@lunatech.nl>
2021-11-18 09:58:41 +0100

    Cleanup some models code and optimize imports

--- app/models/Author.scala
+++ app/models/Author.scala
@@ -1,17 +1,19 @@
 package models
 
-import org.joda.time.{ DateTime, DateTimeZone }
 import play.api.libs.json._
 
 object Author {
-      implicit val authorWrites = Json.writes[Author]
+  implicit val authorWrites: OWrites[Author] = Json.writes[Author]
 }
-case class Author(login: String,
-      avatar_url: String,
-      html_url: String,
-      name: Option[String] = None,
-      email: Option[String] = None,
-      company: Option[String] = None,
-      blog: Option[String] = None,
-      location: Option[String] = None,
-      bio: Option[String] = None)
+
+case class Author(
+                   login: String,
+                   avatar_url: String,
+                   html_url: String,
+                   name: Option[String] = None,
+                   email: Option[String] = None,
+                   company: Option[String] = None,
+                   blog: Option[String] = None,
+                   location: Option[String] = None,
+                   bio: Option[String] = None
+                 )
--- app/models/Post.scala
+++ app/models/Post.scala
@@ -1,16 +1,16 @@
 package models
 
-import org.joda.time.{DateTime, DateTimeZone}
-import org.asciidoctor.Asciidoctor.Factory;
-import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.ast.DocumentHeader;
-import models.Author._
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.Asciidoctor.Factory
+import org.asciidoctor.ast.DocumentHeader
+import org.joda.time.DateTime
 import play.api.libs.json._
+
 import scala.util.control.Exception._
 
 object Post {
-  implicit val postWrites = Json.writes[Post]
-  val asciidoctor = Factory.create()
+  implicit val postWrites: OWrites[Post] = Json.writes[Post]
+  val asciidoctor: Asciidoctor = Factory.create()
 }
 
 case class Post(slug: String, mainImage: String, content: String, author: Option[Author] = None) {
@@ -21,19 +21,19 @@ case class Post(slug: String, mainImage: String, content: String, author: Option
 
   val header: DocumentHeader = Post.asciidoctor.readDocumentHeader(content)
 
-  lazy val authorName: String = header.getAuthor().getFullName()
+  lazy val authorName: String = header.getAuthor.getFullName
 
-  lazy val publicationDate: DateTime = new DateTime(header.getRevisionInfo().getDate())
+  lazy val publicationDate: DateTime = new DateTime(header.getRevisionInfo.getDate)
 
-  val title: String = header.getDocumentTitle().getMain()
+  val title: String = header.getDocumentTitle.getMain
 
-  lazy val lang: String = allCatch.opt(header.getAttributes().get("lang").toString()).getOrElse("en")
+  lazy val lang: String = allCatch.opt(header.getAttributes.get("lang").toString).getOrElse("en")
 
-  lazy val tags: Option[Array[String]] = allCatch.opt(header.getAttributes().get("tags").toString().drop(1).dropRight(1).split(",") :+ lang)
+  lazy val tags: Option[Array[String]] = allCatch.opt(header.getAttributes.get("tags").toString.drop(1).dropRight(1).split(",") :+ lang)
 
-  lazy val excerpt: String = Post.asciidoctor.convert(content.split("\n").drop(6).take(4).mkString(" "), new java.util.HashMap[String, Object]())
+  lazy val excerpt: String = Post.asciidoctor.convert(content.split("\n").slice(6, 10).mkString(" "), new java.util.HashMap[String, Object]())
 
-  def toJson() = {
+  def toJson: JsObject = {
     JsObject(
       Seq(
         "publication_date" -> JsString(publicationDate.toString("dd MMM yyyy")),
@@ -44,9 +44,7 @@ case class Post(slug: String, mainImage: String, content: String, author: Option
         "author" -> JsString(author.map(_.name.getOrElse(authorName)).getOrElse(authorName)),
         "author_name" -> JsString(authorName),
         "author_img" -> JsString(author.map(_.avatar_url).getOrElse("null")),
-        "tags" -> JsArray(tags.getOrElse(Array.empty).toSeq.map(JsString(_)))
+        "tags" -> JsArray(tags.getOrElse(Array.empty).toSeq.map(JsString))
       ))
   }
-
-
 }
